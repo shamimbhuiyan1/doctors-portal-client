b5620dd36745c0dfb19dc218b7c3851a45514110
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-05-21 18:13:36 +0600

    info,info card

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
       {
         doctortheme: {
           primary: "#0FCFEC",
-          secondary: "#f6d860",
-          accent: "red",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
           neutral: "#3d4451",
           "base-100": "#ffffff",
         },
