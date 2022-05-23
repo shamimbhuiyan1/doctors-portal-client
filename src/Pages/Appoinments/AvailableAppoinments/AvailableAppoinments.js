739a101eccad16822a5794dac847702fc71fe6de
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-05-23 18:54:45 +0600

    updated

--- src/Pages/Appoinments/AvailableAppoinments/AvailableAppoinments.js
+++ src/Pages/Appoinments/AvailableAppoinments/AvailableAppoinments.js
@@ -1,25 +1,41 @@
 import { format } from "date-fns";
 import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import Loading from "../../Shared/Loading/Loading";
 import AppoinmentService from "../AppoinmentService/AppoinmentService";
 import BookingModal from "../BookingModal/BookingModal";
 
 const AvailableAppoinments = ({ date }) => {
-  const [services, setServices] = useState([]);
-  const [treatment, setTreatment] = useState(null);
-
   const formattedDate = format(date, "PP");
+  const [treatment, setTreatment] = useState(null);
+  const {
+    data: services,
+    isLoading,
+    refetch,
+  } = useQuery(["available", formattedDate], () =>
+    fetch(`http://localhost:5000/available?date=${formattedDate}`).then((res) =>
+      res.json()
+    )
+  );
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  //iniatially evabe korbo
+
+  /* const [services, setServices] = useState([]);
   useEffect(() => {
     fetch(`http://localhost:5000/available?date=${formattedDate}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, [formattedDate]);
+  }, [formattedDate]); */
   return (
-    <div className="my-10">
+    <div className="my-12">
       <h2 className="text-xl text-secondary text-center my-12">
         Available Appoinments on: {format(date, "PP")}
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {services.map((service) => (
+        {services?.map((service) => (
           <AppoinmentService
             key={service._id}
             service={service}
@@ -32,6 +48,7 @@ const AvailableAppoinments = ({ date }) => {
           date={date}
           treatment={treatment}
           setTreatment={setTreatment}
+          refetch={refetch}
         />
       )}
     </div>
